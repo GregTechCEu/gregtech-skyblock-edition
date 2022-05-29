5e443370ea45db140fe04b177a58515e98b44368
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-05-29 17:57:49 +0100

    Update main.py

--- build/main.py
+++ build/main.py
@@ -20,6 +20,7 @@ def parse_args():
     parser.add_argument("--retries", type=int, default=3, help="download attempts before failure")
     parser.add_argument("--clean", action="store_true", help="clean output dirs")
     parser.add_argument("--dev_build", action="store_true", help="makes a folder with all the files symlinked for development. probally only works on linux")
+    parser.add_argument("--try_server", action="store_true", help="trys to use old way of getting server jars")
     return parser.parse_args()
 
 
@@ -79,27 +80,24 @@ if os.path.isdir(prev):
 if cached > 0:
     print("cached %d mod downloads in %s" % (cached, cachepath))
 
-try:
-    for mod in manifest["externalDeps"]:
-        with open(basePath + "/mods/" + mod["url"].split("/")[-1], "w+b") as jar:
-            for i in range(args.retries + 1):
-                if i == args.retries:
-                    raise Exception("Download failed")
+for mod in manifest["externalDeps"]:
+    with open(basePath + "/mods/" + mod["url"].split("/")[-1], "w+b") as jar:
+        for i in range(args.retries + 1):
+            if i == args.retries:
+                raise Exception("Download failed")
 
-                r = requests.get(mod["url"])
+            r = requests.get(mod["url"])
 
-                hash = hashlib.sha256(jar.read()).hexdigest()
-                if str(hash) == mod["hash"]:
-                    jar.write(r.content)
-                    modlist.append(mod["name"])
-                    print("hash succsessful")
-                    break
-                else:
-                    print("hash unsuccsessful")
-                    print("use", str(hash), "this if it is consistant across runs")
-                    pass
-except:
-    print("no external deps found")
+            hash = hashlib.sha256(jar.read()).hexdigest()
+            if str(hash) == mod["hash"]:
+                jar.write(r.content)
+                modlist.append(mod["name"])
+                print("hash succsessful")
+                break
+            else:
+                print("hash unsuccsessful")
+                print("use", str(hash), "this if it is consistant across runs")
+                pass
 
 for dir in copyDirs:
     try:
@@ -116,101 +114,108 @@ shutil.copy(basePath + "/manifest.json", basePath + "/buildOut/client/manifest.j
 shutil.make_archive(archive, "zip", basePath + "/buildOut/client")
 print('client zip "%s.zip"  made' % (archive))
 
-for mod in manifest["files"]:
-    url = "https://cursemeta.dries007.net/" + str(mod["projectID"]) + "/" + str(mod["fileID"]) + ".json"
-    r = requests.get(url)
-    metadata = json.loads(r.text)
-    modlist.append(metadata["FileName"])
-    modURLlist.append(metadata["DownloadURL"])
-    try:
-        modClientOnly.append(mod["clientOnly"])
-    except:
-        modClientOnly.append(False)
+if args.try_server:
+    for mod in manifest["files"]:
+        url = "https://cursemeta.dries007.net/" + str(mod["projectID"]) + "/" + str(mod["fileID"]) + ".json"
+        r = requests.get(url)
+        metadata = json.loads(r.text)
+        if "name" in mod:
+            name = mod["name"]
+            if name[-4:] != ".jar":
+                name += ".jar"
+            modlist.append(name)
+        else:
+            modlist.append(metadata["FileName"])
+        modURLlist.append(metadata["DownloadURL"])
+        try:
+            modClientOnly.append(mod["clientOnly"])
+        except:
+            modClientOnly.append(False)
 
-print("modlist compiled")
+    print("modlist compiled")
 
-with open(basePath + "/buildOut/modlist.html", "w") as file:
-    data = "<html><body><h1>Modlist</h1><ul>"
-    for mod in modlist:
-        data += "<li>" + mod + "</li>"
-    data += "</ul></body></html>"
-    file.write(data)
+    with open(basePath + "/buildOut/modlist.html", "w") as file:
+        data = "<html><body><h1>Modlist</h1><ul>"
+        for mod in modlist:
+            data += "<li>" + mod.split(".jar")[0] + "</li>"
+        data += "</ul></body></html>"
+        file.write(data)
 
-print("modlist.html done")
+    print("modlist.html done")
 
-shutil.copy(basePath + "/manifest.json", basePath + "/buildOut/server/manifest.json")
-shutil.copy(basePath + "/LICENSE", basePath + "/buildOut/server/LICENSE")
-shutil.copy(basePath + "/launch.sh", basePath + "/buildOut/server/launch.sh")
+    shutil.copy(basePath + "/manifest.json", basePath + "/buildOut/server/manifest.json")
+    shutil.copy(basePath + "/LICENSE", basePath + "/buildOut/server/LICENSE")
+    shutil.copy(basePath + "/launch.sh", basePath + "/buildOut/server/launch.sh")
 
-for dir in serverCopyDirs:
-    try:
-        shutil.copytree(basePath + dir, basePath + "/buildOut/server" + dir)
-    except Exception as e:
-        print("Directory exists, skipping")
-print("directories copied to buildOut/server")
+    for dir in serverCopyDirs:
+        try:
+            shutil.copytree(basePath + dir, basePath + "/buildOut/server" + dir)
+        except Exception as e:
+            print("Directory exists, skipping")
+    print("directories copied to buildOut/server")
 
-for i, mod in enumerate(modURLlist):
-    jarname = mod.split("/")[-1]
-    if (modClientOnly[i] == True):
-        break
+    for i, mod in enumerate(modURLlist):
+        jarname = mod.split("/")[-1]
+        if (modClientOnly[i] == True):
+            break
 
-    if os.path.exists(os.path.join(cachepath, jarname)):
-        shutil.copy2(os.path.join(cachepath, jarname), os.path.join(basePath, "buildOut", "server", "mods", jarname))
-        print("%s loaded from cache" % (mod))
-        continue
+        if os.path.exists(os.path.join(cachepath, jarname)):
+            shutil.copy2(os.path.join(cachepath, jarname), os.path.join(basePath, "buildOut", "server", "mods", jarname))
+            print("%s loaded from cache" % (mod))
+            continue
 
-    with open(basePath + "/buildOut/server/mods/" + jarname, "w+b") as jar:
-        r = requests.get(mod)
-        jar.write(r.content)
-        print(mod + " Downloaded")
-print("Mods Downloaded")
+        with open(basePath + "/buildOut/server/mods/" + jarname, "w+b") as jar:
+            r = requests.get(mod)
+            jar.write(r.content)
+            print(mod + " Downloaded")
+    print("Mods Downloaded")
 
-with open(basePath + "/buildOut/server/forge-installer.jar", "w+b") as jar:
-    forgeVer = manifest["minecraft"]["modLoaders"][0]["id"].split("-")[-1]
-    mcVer = manifest["minecraft"]["version"]
-    url = (
-        "https://maven.minecraftforge.net/net/minecraftforge/forge/"
-        + mcVer
-        + "-"
-        + forgeVer
-        + "/forge-"
-        + mcVer
-        + "-"
-        + forgeVer
-        + "-installer.jar"
-    )
-    r = requests.get(url)
-    jar.write(r.content)
-print("Forge installer Downloaded")
-
-# TODO: make a portable version between versions
-
-vanilla = basePath + "/buildOut/server/minecraft_server.1.12.2.jar"
-
-if not os.path.isfile(vanilla):
-    with open(basePath + "/buildOut/server/minecraft_server.1.12.2.jar", "w+b") as jar:
-        url = "https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar"
+    with open(basePath + "/buildOut/server/forge-installer.jar", "w+b") as jar:
+        forgeVer = manifest["minecraft"]["modLoaders"][0]["id"].split("-")[-1]
+        mcVer = manifest["minecraft"]["version"]
+        url = (
+            "https://maven.minecraftforge.net/net/minecraftforge/forge/"
+            + mcVer
+            + "-"
+            + forgeVer
+            + "/forge-"
+            + mcVer
+            + "-"
+            + forgeVer
+            + "-installer.jar"
+        )
         r = requests.get(url)
         jar.write(r.content)
-    print("Vanilla Downloaded")
+    print("Forge installer Downloaded")
 
-subprocess.run(["java", "-jar", "forge-installer.jar", "--installServer"], cwd=basePath + "/buildOut/server/")
-print("Forge Installed")
+    # TODO: make a portable version between versions
 
-try:
-    os.remove(basePath + "/buildOut/server/forge-installer.jar")
-except Exception as e:
-    print("Couldn't delete forge-installer.jar: %s" % (e))
-try:
-    os.remove(basePath + "/buildOut/server/forge-installer.jar.log")
-except Exception as e:
-    print("Couldn't delete forge-installer.jar.log: %s" % (e))
+    vanilla = basePath + "/buildOut/server/minecraft_server.1.12.2.jar"
 
-archive = "buildOut/server"
-if sha:
-    archive = "%s-%s" % (archive, sha)
-shutil.make_archive(archive, "zip", basePath + "/buildOut/server")
-print('server zip "%s.zip" made' % (archive))
+    if not os.path.isfile(vanilla):
+        with open(basePath + "/buildOut/server/minecraft_server.1.12.2.jar", "w+b") as jar:
+            url = "https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar"
+            r = requests.get(url)
+            jar.write(r.content)
+        print("Vanilla Downloaded")
+
+    subprocess.run(["java", "-jar", "forge-installer.jar", "--installServer"], cwd=basePath + "/buildOut/server/")
+    print("Forge Installed")
+
+    try:
+        os.remove(basePath + "/buildOut/server/forge-installer.jar")
+    except Exception as e:
+        print("Couldn't delete forge-installer.jar: %s" % (e))
+    try:
+        os.remove(basePath + "/buildOut/server/forge-installer.jar.log")
+    except Exception as e:
+        print("Couldn't delete forge-installer.jar.log: %s" % (e))
+
+    archive = "buildOut/server"
+    if sha:
+        archive = "%s-%s" % (archive, sha)
+    shutil.make_archive(archive, "zip", basePath + "/buildOut/server")
+    print('server zip "%s.zip" made' % (archive))
 
 if (args.dev_build):
     mkdirs(basePath + "/buildOut/mmc/minecraft")
